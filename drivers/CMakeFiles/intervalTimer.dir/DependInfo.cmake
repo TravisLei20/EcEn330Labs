baee91e503563d16f2185977f3592b1c8e895e29
TravisLei20 <80382220+TravisLei20@users.noreply.github.com>
2021-11-23 12:16:38 -0700

    Add files via upload

---
+++ drivers/CMakeFiles/intervalTimer.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/fsd/leicr/ecen330/drivers/intervalTimer.c" "/fsd/leicr/ecen330/build_emu/drivers/CMakeFiles/intervalTimer.dir/intervalTimer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "QT_NO_VERSION_TAGGING"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "../drivers"
+  "../platforms/emulator/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
